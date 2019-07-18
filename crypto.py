124980d27993de106b763508a6c71ea188fee7fa
Audrey Xie <aaxxie13@gmail.com>
2019-07-18 12:11:16 -0700

    added error message

--- crypto.py
+++ crypto.py
@@ -2,9 +2,27 @@ import cryptocompare
 
 # will add more cool stuff later #
 
+coins = ['BTC', 'ETH', 'XMR', 'NEO']
+currencies = ['EUR', 'USD', 'GBP']
+
 def message(text):
-    name = text[13:16]
-    currency = text[20:23]
-    data = cryptocompare.get_price(name, currency)
-    price = data[name][currency]
-    return name + " in " + currency + ": " + str(price)
+
+    if any(word in text.split() for word in coins) and any(word in text.split() for word in currencies):
+        name = text[13:16]
+        currency = text[20:23]
+        data = cryptocompare.get_price(name, currency)
+        price = data[name][currency]
+        return name + " in " + currency + ": " + str(price)
+    else:
+        return "These are the supported coins:\n" \
+                "\n" \
+                "BTC : bitcoin\n" \
+                "ETH : ethereum\n" \
+                "XMR : monero\n" \
+                "NEO : NEO\n" \
+                "\n" \
+                "These are the supported currencies:\n" \
+                "\n" \
+                "EUR : euro\n" \
+                "USD : us dollar\n" \
+                "GBR : pound sterling\n"
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import messages
 
-reply = messages.processMessage("get price of BTC in USD")
+reply = messages.processMessage("get price of DOGE in USD")
 if reply:
     print(reply)
